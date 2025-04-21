ccf2384de050529f191cd1c8c555b22b8835d0a3
unknown <ce.rumiakter@gmail.com>
2025-04-21 18:32:27 +0600

    add footer section

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='bg-[#527A9A]'>
+            <footer className="footer sm:footer-horizontal container mx-auto text-neutral-content p-10">
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About Us</a>
+                    <a className="link link-hover">Blog</a>
+                    <a className="link link-hover">Career</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Contact</h6>
+                    <a className="link link-hover">Address: Mohammadpur, Dhaka</a>
+                    <a className="link link-hover">Phone: ++88012345678976</a>
+                    <a className="link link-hover">Email- info@shop.com</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Important Links</h6>
+                    <a className="link link-hover">Shop</a>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Terms & Condition</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Follow Us</h6>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Instagram</a>
+                    <a className="link link-hover">Youtube</a>
+                </nav>
+            </footer>
         </div>
     );
 };

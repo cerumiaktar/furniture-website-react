4b9e8a26c518fdf2de9662036c43601b64c5c243
unknown <ce.rumiakter@gmail.com>
2025-09-16 10:29:05 +0600

    update blog pages

--- src/pages/AllBlogs/AllBlog.jsx
+++ src/pages/AllBlogs/AllBlog.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 const AllBlog = ({ allBlog }) => {
     const { img, title, short_description } = allBlog;
     return (
-        <div className="card bg-base-100 shadow-sm">
+        <div className="card bg-base-100 dark:bg-white shadow-sm">
             <figure>
                 <img className="w-full"
                     src={img}
                     alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">{title}</h2>
-                <p>{short_description}</p>
+                <h2 className="card-title dark:text-black">{title}</h2>
+                <p className='text-[#333333]'>{short_description}</p>
                 <div className="card-actions ">
-                    <button className="btn bg-[#527A9A] text-white text-base w-full">Read More</button>
+                    <button className="btn bg-[#527A9A] text-white text-base w-full dark:border-none">Read More</button>
                 </div>
             </div>
         </div>

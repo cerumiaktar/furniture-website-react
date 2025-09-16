5816c46bd4869e69f3d73188cfb86f0d9f77b3ad
unknown <ce.rumiakter@gmail.com>
2025-09-16 11:04:42 +0600

    update register page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
                                 <label className="label dark:text-[#333333]">Password</label>
                                 <input type="password" className="input dark:bg-white dark:text-[#333333] dark:border-[#d3d3d3]" placeholder="Password" />
                                 <div><a className="link link-hover dark:text-red-500">Forgot password?</a></div>
-                                <button className="btn bg-[#527A9A] mt-4 dark:border-none">Login</button>
+                                <button className="btn bg-[#527A9A] text-white mt-4 dark:border-none">Login</button>
                             </fieldset>
                         </div>
                         <p className="px-6 mb-6 dark:text-[#333333]">Dont have an account? <Link className="text-blue-800 font-medium" to='/register'>Register</Link></p>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,26 +5,26 @@ const Register = () => {
     return (
         <div>
             <div>
-                <div className="hero bg-base-200 min-h-screen">
+                <div className="hero bg-base-200 dark:bg-white min-h-screen">
                     <div className="hero-content flex-col">
                         <div className="text-center lg:text-left">
-                            <h1 className="text-4xl font-bold">Register now!</h1>
+                            <h1 className="text-4xl font-bold dark:text-black">Register now!</h1>
                         </div>
-                        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card bg-base-100 dark:bg-white w-full max-w-sm shrink-0 shadow-2xl">
                             <div className="card-body">
                                 <fieldset className="fieldset">
-                                    <label className="label">Name</label>
-                                    <input type="text" className="input" placeholder="Name" />
-                                    <label className="label">Email</label>
-                                    <input type="email" className="input" placeholder="Email" />
-                                    <label className="label">Password</label>
-                                    <input type="password" className="input" placeholder="Password" />
-                                    <label className="label">Confirm Password</label>
-                                    <input type="password" className="input" placeholder="Confirm Password" />
-                                    <button className="btn btn-neutral mt-4">Register</button>
+                                    <label className="label dark:text-[#333333]">Name</label>
+                                    <input type="text" className="input dark:bg-white dark:text-[#333333] dark:border-[#d3d3d3]" placeholder="Name" />
+                                    <label className="label dark:text-[#333333]">Email</label>
+                                    <input type="email" className="input dark:bg-white dark:text-[#333333] dark:border-[#d3d3d3]" placeholder="Email" />
+                                    <label className="label dark:text-[#333333]">Password</label>
+                                    <input type="password" className="input dark:bg-white dark:text-[#333333] dark:border-[#d3d3d3]" placeholder="Password" />
+                                    <label className="label dark:text-[#333333]">Confirm Password</label>
+                                    <input type="password" className="input dark:bg-white dark:text-[#333333] dark:border-[#d3d3d3]" placeholder="Confirm Password" />
+                                    <button className="btn btn-neutral mt-4 bg-[#527A9A] text-white border-none dark:border-none">Register</button>
                                 </fieldset>
                             </div>
-                            <p className="px-6 mb-6">You have an account? <Link className="text-blue-800 font-medium" to='/login'>Login</Link></p>
+                            <p className="px-6 mb-6 dark:text-[#333333]">You have an account? <Link className="text-blue-800 font-medium" to='/login'>Login</Link></p>
                         </div>
                     </div>
                 </div>

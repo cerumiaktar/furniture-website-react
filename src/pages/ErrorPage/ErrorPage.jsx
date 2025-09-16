62fc52386c8c14bdf648e0966d65229d4ce66980
unknown <ce.rumiakter@gmail.com>
2025-09-16 10:36:19 +0600

    update errorpage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
         <div className='mt-20 mb-20 space-y-4'>
             <img className='mx-auto' src="https://i.ibb.co.com/Txs2B5Wp/error-1.png" alt="" />
             <div className='text-center'>
-                <Link to='/'><button className='btn bg-white text-black'>Go to Homepage</button></Link>
+                <Link to='/'><button className='btn bg-[#527A9A] text-white dark:bg-white dark:text-black'>Go to Homepage</button></Link>
             </div>
         </div>
     );

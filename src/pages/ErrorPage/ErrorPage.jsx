f4eedf1987457fe2ffa8e45f1958fc52ccddcdfb
unknown <ce.rumiakter@gmail.com>
2025-09-16 10:33:27 +0600

    update error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className='mt-20'>
+        <div className='mt-20 mb-20 space-y-4'>
             <img className='mx-auto' src="https://i.ibb.co.com/Txs2B5Wp/error-1.png" alt="" />
+            <div className='text-center'>
+                <Link to='/'><button className='btn bg-white text-black'>Go to Homepage</button></Link>
+            </div>
         </div>
     );
 };

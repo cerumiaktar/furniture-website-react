f118def01b6f673406c13c535d65ec97868e15b8
unknown <ce.rumiakter@gmail.com>
2025-10-18 13:33:14 +0600

    add contact

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -15,7 +16,7 @@ const Banner = () => {
                     <div className="max-w-4xl">
                         <h1 className="mb-5 text-5xl font-bold">Elevate Your Home Decor with Our 
                         Premium Furniture Collection</h1>
-                        <button className="btn text-lg dark:bg-white dark:text-black">Contact Us</button>
+                        <Link to='/contact'><button className="btn text-lg dark:bg-white dark:text-black">Contact Us</button></Link>
                     </div>
                 </div>
             </div>
